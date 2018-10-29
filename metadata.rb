a9d3bb32ee79be2d34e5f02fce2c3d8ef426f81e
Jonathan Sloan <jsloan117@gmail.com>
2018-10-29 14:14:02 -0500

    update metadata, create another recipe

--- metadata.rb
+++ metadata.rb
@@ -3,20 +3,15 @@ maintainer 'Jonathan Sloan'
 maintainer_email 'jsloan117@gmail.com'
 license 'GPL-3.0'
 description 'Installs/Configures hms'
-long_description 'Installs/Configures hms'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_version '>= 14.1.12' if respond_to?(:chef_version)
 version '0.1.0'
-chef_version '>= 13.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://github.com/jsloan117/hms/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-source_url 'https://github.com/jsloan117/hms'
+issues_url 'https://github.com/jsloan117/hms/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/jsloan117/hms' if respond_to?(:source_url)
 
 supports 'centos', '>= 7.0'
+
+depends 'docker', '>= 4.6.7'
+depends 'yum-epel', '>= 3.3.0'
+depends 'yumgroup', '>= 0.6.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, Jonathan Sloan, GPL-3.0.
+error = ''
+
+def recipe_list
+  include_recipe 'yum-epel'
+  include_recipe 'yumgroup'
+  include_recipe 'hms::server_setup'
+end
+
+if node['platform'] == 'centos' && node['platform_version'].to_i >= 7
+  recipe_list
+else
+  error = 'Please use a supported OS and version'
+end
+raise error unless error.empty?
